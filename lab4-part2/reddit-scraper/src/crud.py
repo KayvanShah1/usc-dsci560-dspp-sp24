ba134de6a25f589a70d45bb928fe1c536c37b2f1
Kayvan Shah <kayvan.shah@usc.edu>
2024-02-15 00:20:21 -0800

    added a bulk create operation

--- lab4-part2/reddit-scraper/src/crud.py
+++ lab4-part2/reddit-scraper/src/crud.py
@@ -1,3 +1,4 @@
+from typing import List
 import model
 import schema
 from settings import get_logger
@@ -18,6 +19,25 @@ def create(request: schema.RedditPostModelNew, db: Session):
     db.refresh(new_post)
     logger.info(f"Successfully added post data with id '{request.id}' to the database")
     return new_post
+
+
+def bulk_create(requests: List[schema.RedditPostModelNew], db: Session):
+    new_posts = []
+
+    for np in requests:
+        new_post = model.RedditPostNew(**np.model_dump())
+        new_posts.append(new_post)
+
+    db.bulk_save_objects(new_posts)
+    db.commit()
+    # db.refresh(new_post)
+    # Extract the IDs of the newly inserted posts
+
+    for post in new_posts:
+        logger.info(f"Successfully added post data with id '{post.id}' to the database")
+
+    logger.info(f"Successfully added '{len(new_posts)}' posts to the database")
+    return new_posts
 
 
 def post_exists(id: str, db: Session):
